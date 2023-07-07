1788d5dd3b1c595c187f141e2d18f1e2e73c470e
YRodas <yrodas-2018481@kinal.edu.gt>
2023-07-06 18:23:59 -0600

    Venta Online

---
+++ VentaOnline/src/product/product.routes.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const productController = require('./product.controller');
+const express = require('express');
+const api = express.Router();
+const { ensureAuth, isAdmin } = require('../services/authenticated')
+
+
+api.get('/', productController.test);
+api.post('/register',[ensureAuth, isAdmin], productController.register);
+api.put('/update/:id',[ensureAuth, isAdmin], productController.update);
+api.delete('/delete/:id',[ensureAuth, isAdmin], productController.delete);
+api.get('/get', ensureAuth, productController.getProducts);
+api.get('/search', ensureAuth ,productController.search);
+api.get('/searchByCategory',ensureAuth ,productController.searchByCategory);
+api.get('/getOutOfStockProducts', [ensureAuth, isAdmin] ,productController.getOutOfStockProducts)
+api.get('/getBestSellingProducts',ensureAuth ,productController.getBestSellingProducts)
+
+module.exports = api;
